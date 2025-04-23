b032092826914b75e0d4325c47c9fba4a16395aa
Ronald-0001 <106472835+Ronald-0001@users.noreply.github.com>
2025-04-23 14:29:09 +0200

    Initial commit

---
+++ src/main/java/net/mao/rw/ResourceWorldClient.java
@@ -0,0 +1,10 @@
+package net.mao.rw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResourceWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mao/rw/ResourceWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mao.rw;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ResourceWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
